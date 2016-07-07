3f610cf72002beb11e73a47c2f118e35ff2d9575
croxis <croxis@gmail.com>
2016-07-07 13:27:06 -0700

    Improved setup for windows and pip installs

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup, find_packages
 
+import os
+import subprocess
+import sys
+print("PBR pipeline will now be setup for your system.")
+print("If you would like to compile the C++ addon please follow these instructions before continuing:")
+print("https://github.com/tobspr/RenderPipeline/wiki/Building-the-CPP-Modules")
+input("Press enter to continue")
+os.chdir('spacedrive/renderpipeline')
+subprocess.call([sys.executable, "setup.py"])
+os.chdir('../..')
+
 setup(name='SpaceDrive',
       version='2.0',
       description='Panda3D Space Game Engine. Requires sandbox',
       author='croxis',
       author_email='croxis@gmail.com',
       url='http://croxis.net/',
+      install_requires=[
+            'pyyaml'
+      ],
       package_data={
           ''
           'spacedrive': ['Shader/Planet/*.glsl',
